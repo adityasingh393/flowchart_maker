f903ae7fa8edd4c62dc18234a5a485c9f1157b1d
Aditya Singh <aditya.singh@binmile.com>
2024-09-04 13:11:59 +0530

    added custom shapes

--- src/Components/Nodes/CircularNode.tsx
+++ src/Components/Nodes/CircularNode.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Handle, Position } from '@xyflow/react';
+import { Handle, Position } from "@xyflow/react";
 
 const CircularNode = ({ id, data }: any) => {
   return (
@@ -7,34 +6,34 @@ const CircularNode = ({ id, data }: any) => {
       style={{
         width: 200,
         height: 90,
-        borderRadius: '50%',
-        border: '1px solid black',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'white',
+        borderRadius: "50%",
+        border: "1px solid black",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "white",
       }}
     >
       {data.label}
       <Handle
         type="source"
         position={Position.Top}
-        style={{ top: -5, left: '50%', transform: 'translateX(-50%)' }}
+        style={{ top: -5, left: "50%", transform: "translateX(-50%)" }}
       />
       <Handle
         type="source"
         position={Position.Bottom}
-        style={{ bottom: -5, left: '50%', transform: 'translateX(-50%)' }}
+        style={{ bottom: -5, left: "50%", transform: "translateX(-50%)" }}
       />
       <Handle
         type="target"
         position={Position.Left}
-        style={{ top: '50%', left: -5, transform: 'translateY(-50%)' }}
+        style={{ top: "50%", left: -5, transform: "translateY(-50%)" }}
       />
       <Handle
         type="target"
         position={Position.Right}
-        style={{ top: '50%', right: -5, transform: 'translateY(-50%)' }}
+        style={{ top: "50%", right: -5, transform: "translateY(-50%)" }}
       />
     </div>
   );
